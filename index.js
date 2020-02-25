11389bef2710a9028a7b88c8d647b91710808bff
Emiliano <emiliano9669@gmail.com>
2020-02-25 01:18:52 -0300

    paginado terminado

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var MovieIndex = 0;
 
+Check_IfNeedTo_Hide_BackButton();
+
 $("select").change(function() {
   MovieIndex = 0;
-  console.log($("select").val());
   GetMovies();
 });
 
 function GetMovies() {
-  console.log("MovieIndex vale: " + MovieIndex);
   var parametros = {
     Genero: $("select").val(),
     MovieNro: MovieIndex
@@ -17,7 +17,27 @@ function GetMovies() {
     url: "indexAux.php",
     type: "post",
     success: function(response) {
-      $("main").html(response);
+      if (response != "") {
+        $("main").html(response);
+      } else {
+        alert("No existen mas películas en el catálogo");
+      }
     }
   });
 }
+
+$("#Next").click(function() {
+  console.log("Next was clicked");
+  $("#Back").show();
+});
+
+function Check_IfNeedTo_Hide_BackButton() {
+  if (MovieIndex == 0) {
+    $("#Back").hide();
+  }
+}
+
+$("#Back").click(function() {
+  console.log("Back was clicked");
+  Check_IfNeedTo_Hide_BackButton();
+});
